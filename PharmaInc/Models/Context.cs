e361d8d2a8c20c179c3d831fe05c381d4a6c18b6
Picos88 <pietrocosentino88@gmail.com>
2021-11-08 19:47:50 -0300

    Adicionar arquivos de projeto.

---
+++ PharmaInc/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PharmaInc.Models
+{
+    public class Context : DbContext
+    {
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public virtual DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=PharmaInc;Integrated Security=True");
+        }
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
